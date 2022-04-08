83ab89f866777f56d8a59dc208e30b21e65024da
judith lourde <judith.lourde@gmail.com>
2022-04-09 00:19:13 +0200

    Add Header with navogation not responsive yet

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,54 +1,54 @@
 <template>
-     <header class="header">
-         <div class="header__container">
-            <nav class="header__container-navbar">
-               <!-- Main Menu -->
-               <div class="navigation-desktop">
-                  <ul class="navigation-desktop__list">
-                     <li>
-                        <a href="#" class="about_link">
-                           <span>About Us</span>
-                        </a>
-                     </li>
+   <header class="header">
+      <div class="header__container">
+         <nav class="header__container-navbar">
+            <!-- Main Menu -->
+            <div class="navigation-desktop">
+               <ul class="navigation-desktop__list">
+                  <li>
+                     <a href="#" class="about_link">
+                        <span>About Us</span>
+                     </a>
+                  </li>
 
-                     <li>
-                        <a href="#products" class="product_link">
-                           <span>Products</span>
-                        </a>
-                     </li>
+                  <li>
+                     <a href="#products" class="product_link">
+                        <span>Products</span>
+                     </a>
+                  </li>
 
-                     <li>
-                        <a href="#" class="contact_link">
-                           <span>Contact Us</span>
-                        </a>
-                     </li>
-                  </ul>
+                  <li>
+                     <a href="#" class="contact_link">
+                        <span>Contact Us</span>
+                     </a>
+                  </li>
+               </ul>
+            </div>
+            <!-- Hamburger menu for mobile -->
+            <div class="navigation-mobile">
+               <img src="/svg/hamburger.svg" alt="icon for hamburger menu">
+            </div>
+
+            <!-- Logo -->
+            <a href="#" class="coffee-logo">
+               <span><img src="/images/logo.png" alt="Judith's Coffee logo" /></span>
+            </a>
+
+            <div class="header_account">
+               <div class="cart">
+                  <!-- <a href="#cart_container" class="jar_button"> -->
+                     <span class="view_cart">
+                        <img src="/images/jar.png" alt="cart-jar-images" />
+
+                        <span class="count">0</span>
+                     </span>
+
+                     
+                  <!-- </a> -->
                </div>
-
-               <div class="navigation-mobile">
-                  <img src="" alt="">
-               </div>
-
-               <!-- Logo -->
-               <a href="#" class="coffee-logo">
-                  <span><img src="/images/logo.png" alt="Judith's Coffee logo" /></span>
-               </a>
-
-               <div class="header_account">
-                  <div class="cart">
-                     <!-- <a href="#cart_container" class="jar_button"> -->
-                        <span class="view_cart">
-                           <img src="/images/jar.png" alt="cart-jar-images" />
-
-                           <span class="count">0</span>
-                        </span>
-
-                        
-                     <!-- </a> -->
-                  </div>
-               </div>
-            </nav>
-         </div>
+            </div>
+         </nav>
+      </div>
       </header>
 </template>
 
@@ -66,7 +66,7 @@
    }
 
    .header__container {
-      padding: 10px 48px;
+      padding: 10px 38px;
    }
 
    .header__container-navbar {
@@ -74,40 +74,20 @@
    }
 
    .navigation-desktop {
-      padding: 10px;
-      flex: 1;
-      font-size: var(--points);
-      font-weight: 500;
-      text-align: center;
-      line-height: 1.8;
+      display: none;
    }
 
-   .navigation-desktop__list {
-      display: flex;
-   }
-
-   .navigation-desktop__list li {
-      list-style: none;
-      margin: 0 8px;
-   }
-
-   .navigation-desktop__list li a {
-      display: inline-block;
-      text-decoration: none;
-      color: inherit;
-   }
-
-   .navigation-desktop__list li a span {
-      display: inline-block;
-      padding: 8px 16px;
+   .navigation-mobile {
+      display: block;
    }
 
    .coffee-logo span {
       display: block;
+      padding: 0 0 0 55px;
    }
 
    .coffee-logo span img {
-      width: 20vw;
+      width: 40vw;
    }
 
    .header_account {
@@ -116,12 +96,13 @@
       justify-content: flex-end;
       flex: 1;
    }
+
    .view_cart {
       position: relative;
    }
 
    .view_cart img {
-      width: 2.6vw;
+      width: 5.6vw;
       /* position: relative; */
    }
 
@@ -136,5 +117,67 @@
       font-size: var(--points);
       border-radius: 100%;
       border-style: none;
+   }
+
+   @media screen and (min-width: 968px) { 
+      .header__container {
+         padding: 10px 48px;
+      }
+
+      .navigation-desktop {
+         display: block;
+         padding: 10px;
+         flex: 1;
+         font-size: var(--heading);
+         font-weight: 500;
+         text-align: center;
+         line-height: 1.8;
+      }
+
+      .navigation-mobile {
+         display: none;
+      }
+
+      .navigation-desktop__list {
+         display: flex;
+      }
+
+      .navigation-desktop__list li {
+         list-style: none;
+         margin: 0 8px;
+      }
+
+      .navigation-desktop__list li a {
+         display: inline-block;
+         text-decoration: none;
+         color: inherit;
+      }
+
+      .navigation-desktop__list li a span {
+         display: inline-block;
+         padding: 8px 16px;
+      }
+
+      .coffee-logo span img {
+         width: 20vw;
+      }
+
+      .view_cart img {
+         width: 2.6vw;
+         /* position: relative; */
+      }
+
+      .count {
+         position: absolute;
+         left: 24px;
+         bottom: 30px;
+         background-color: #EE7675;
+         color: #fff;
+         /* border-radius: 100%; */
+         padding: 4px 8px;
+         font-size: var(--points);
+         border-radius: 100%;
+         border-style: none;
+      }
    }
 </style>
