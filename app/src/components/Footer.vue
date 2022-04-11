8bd37105402d56d39f306b07cc9deec9049c39c0
judith lourde <judith.lourde@gmail.com>
2022-04-11 23:23:16 +0200

    Fix margin in footer

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -33,11 +33,12 @@
 <style>
     footer {
         width: 100%;
+        /* height: 100%; */
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        margin-top: 140px;
+        margin-top: 200px;
         background-color: var(--footer-color);
     }
 
