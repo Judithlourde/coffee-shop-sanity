692c0e0b067898dad198d997b8f51280f257bd47
judith lourde <judith.lourde@gmail.com>
2022-04-11 14:55:17 +0200

    Change class name in Header Component

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,76 +1,68 @@
 <template>
-   <header class="header">
-      <div class="header__container">
-         <nav class="header__container-navbar">
-            <!-- Main Menu -->
-            <div class="navigation-desktop">
-               <ul class="navigation-desktop__list">
-                  <li>
-                     <a href="#" class="about_link">
-                        <span>About Us</span>
-                     </a>
-                  </li>
+   <header>
+      <!-- Navigation Menu -->
+      <nav>
+         <ul class="navigation-desktop">
+            <li>
+               <a href="#">
+                  <span>About Us</span>
+               </a>
+            </li>
 
-                  <li>
-                     <a href="#products" class="product_link">
-                        <span>Products</span>
-                     </a>
-                  </li>
+            <li>
+               <a href="#products">
+                  <span>Products</span>
+               </a>
+            </li>
 
-                  <li>
-                     <a href="#" class="contact_link">
-                        <span>Contact Us</span>
-                     </a>
-                  </li>
-               </ul>
-            </div>
-            <!-- Hamburger menu for mobile -->
-            <div class="navigation-mobile">
-               <img src="/svg/hamburger.svg" alt="icon for hamburger menu">
-            </div>
+            <li>
+               <a href="#">
+                  <span>Contact Us</span>
+               </a>
+            </li>
+         </ul>
+   
+         <!-- Hamburger menu for mobile -->
+         <div class="navigation-mobile">
+            <img src="/svg/hamburger.svg" alt="icon for hamburger menu">
+         </div>
 
-            <!-- Logo -->
-            <a href="#" class="coffee-logo">
-               <span><img src="/images/logo.png" alt="Judith's Coffee logo" /></span>
-            </a>
+         <!-- Logo -->
+         <a href="#" class="coffee-logo">
+            <span><img src="/images/logo.png" alt="Judith's Coffee logo" /></span>
+         </a>
+           
+         <div class="cart">
+            <!-- <a href=""> -->
+               <span class="cart__jar-button">
+                  <img src="/images/jar.png" alt="cart-jar-images" />
 
-            <div class="header_account">
-               <div class="cart">
-                  <!-- <a href="#cart_container" class="jar_button"> -->
-                     <span class="view_cart">
-                        <img src="/images/jar.png" alt="cart-jar-images" />
-
-                        <span class="count">0</span>
-                     </span>
-
-                     
-                  <!-- </a> -->
-               </div>
-            </div>
-         </nav>
-      </div>
-      </header>
+                  <span class="cart__jar-button-count">0</span>
+               </span>                     
+            <!-- </a> -->
+         </div>
+      </nav>
+   </header>
 </template>
 
 <style>
    /* Header Section-Navigation-bar */
-   .header {
+   header {
       position: fixed;
       top: 0;
       left: 0;
       width: 100vw;
-      height: 14vh;
+      height: 100px;
       z-index: 200;
       border-bottom: solid 1px var(--border-color);
-      background-color: #fff;
+      padding: 30px 25px;
    }
 
-   .header__container {
-      padding: 10px 38px;
-   }
-
-   .header__container-navbar {
+   nav {
       display: flex;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
    }
 
    .navigation-desktop {
@@ -83,98 +75,82 @@
 
    .coffee-logo span {
       display: block;
-      padding: 0 0 0 55px;
    }
 
    .coffee-logo span img {
-      width: 40vw;
+      width: 60vw;
    }
 
-   .header_account {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      flex: 1;
-   }
-
-   .view_cart {
+   .cart__jar-button {
       position: relative;
    }
 
-   .view_cart img {
-      width: 5.6vw;
-      /* position: relative; */
+   .cart__jar-button img {
+      width: 6vw;
    }
 
-   .count {
+   .cart__jar-button-count {
       position: absolute;
-      left: 24px;
-      bottom: 30px;
+      left: 12px;
+      bottom: 25px;
       background-color: #EE7675;
       color: #fff;
-      /* border-radius: 100%; */
-      padding: 4px 8px;
+      padding: 1px 6px;
       font-size: var(--points);
       border-radius: 100%;
       border-style: none;
    }
 
    @media screen and (min-width: 968px) { 
-      .header__container {
+      header {
          padding: 10px 48px;
-      }
-
-      .navigation-desktop {
-         display: block;
-         padding: 10px;
-         flex: 1;
-         font-size: var(--heading);
-         font-weight: 500;
-         text-align: center;
-         line-height: 1.8;
       }
 
       .navigation-mobile {
          display: none;
       }
 
-      .navigation-desktop__list {
+      .navigation-desktop {
          display: flex;
+         font-size: var(--heading);
+         font-weight: 500;
       }
 
-      .navigation-desktop__list li {
+      .navigation-desktop li {
          list-style: none;
-         margin: 0 8px;
+         margin: 0 30px;
       }
 
-      .navigation-desktop__list li a {
+      .navigation-desktop li a {
          display: inline-block;
          text-decoration: none;
          color: inherit;
       }
 
-      .navigation-desktop__list li a span {
+      .navigation-desktop li a span {
          display: inline-block;
          padding: 8px 16px;
       }
 
+      .coffee-logo {
+         flex: 1;
+      }
+
       .coffee-logo span img {
-         width: 20vw;
+         width: 300px;
       }
 
-      .view_cart img {
-         width: 2.6vw;
-         /* position: relative; */
+      .cart__jar-button img {
+         width: 30px;
       }
 
-      .count {
+      .cart__jar-button-count {
          position: absolute;
-         left: 24px;
+         left: 20px;
          bottom: 30px;
          background-color: #EE7675;
          color: #fff;
-         /* border-radius: 100%; */
-         padding: 4px 8px;
+         padding: 1px 6px;
          font-size: var(--points);
          border-radius: 100%;
          border-style: none;
