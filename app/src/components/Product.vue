d9746215c79a6c4756b2fe1d81ff990496d700df
judith lourde <judith.lourde@gmail.com>
2022-04-11 23:25:00 +0200

    Fix benefits icons in product page

--- app/src/components/Product.vue
+++ app/src/components/Product.vue
@@ -1,15 +1,54 @@
 <template>
-	<section class="product">
-		<div v-for="product in result" :key="product._id">
-			<h1>{{ product.title }}</h1>
-
+	<section>
+		<div v-for="product in result" :key="product._id" class="product">
 			<div class="product__image">
 				<img :src="product.image.asset.url" :alt="product.image.caption">
-				<p>{{ product.description }}</p>
+			</div>
+
+			<div class="product__description">
+				<h1>{{ product.title }}</h1>
+
+				<div>
+					<h3>Flavoured Instant Coffee</h3>
+					<p>{{ product.description }}</p>
+				</div>
+				
+				<div>
+					<h3>INGREDIENTS</h3>
+					<p>{{ product.ingredients }}</p>
+				</div>
+				
+				<button>ADD TO BASKET</button>
+			</div>
+		</div>
+
+		<div class="benefits-icons">
+			<div>
+				<img src="/images/100-percent-arabica-coffee.png" alt="100-percent-arabica coffee icon">
+				<p>100% Arabica Coffee</p>
 			</div>
 			
+			<div>
+				<img src="/images/no-added-sugar.png" alt="No added sugar icon">
+				<p>No added sugar</p>
+			</div>
+			
+			<div>
+				<img src="/images/lactose-gluten-free.png" alt="Lactose and gluten free icon">
+				<p>Lactose & gluten free</p>
+			</div>
+			
+			<div>
+				<img src="/images/vegan-friendly.png" alt="Vegan friendly icon">
+				<p>Vegan friendly</p>
+			</div>
+			
+			<div>
+				<img src="/images/low-calories-a-cup.png" alt="Low calories icon">
+				<p>Only 4 calories a cup</p>
+			</div>
 		</div>
-	</section> 
+	</section>
 </template>
 
 <script>
@@ -54,8 +93,7 @@
                         }
                     }
                 }
-			`
-				
+			`			
 			const params = { 
                 slug: this.$route.params.productSlug 
             };
@@ -74,12 +112,87 @@
 <style>
 	.product {
 		position: relative;
-		top: 130px;
-
+		top: 80px;
+		width: 100%;
+		display: grid;
+		grid-template-columns: 1fr;
 	}
 
-	.product__image {
+	.product__description {
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
+		margin: 0 30px;
+	}
+
+	.product__description h1 {
+		z-index: 1;
+		color: var(--white);
+        background-color: var(--black);
+		padding: var(--padding-small);
+		margin-bottom: var(--bottom-small);
+	}
+
+	.product__description h3,
+	.product__description p {
+		margin-bottom: var(--bottom-xsmall);
+	}
+
+	.product__description button {
+		color: var(--white);
+        background-color: var(--highlight);
+        padding: var(--padding-small);
+	}
+
+	.benefits-icons {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		grid-gap: 10px;
+		margin: 100px 40px;
+	}
+
+	.benefits-icons div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	
+	.benefits-icons img {
+		object-fit: contain;
+		padding: 30px;
+		width: 100%;
+	}
+	@media screen and (min-width: 968px) {
+		.product {
+			grid-template-columns: repeat(2, 1fr);
+		}
+
+		.product__image {
+			margin: 40px;
+		}
+
+		.product__image img {
+			width: 100%;
+		}
+
+		.product__description {
+			display: flex;
+			justify-content: space-evenly;
+			align-items: flex-start;
+			margin: 60px;
+		}
+
+		.benefits-icons {
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			width: 100%;
+			/* padding: 0 100px; */
+		}
+
+		.benefits-icons div img {
+			width: 220px;
+		}
 	}
 </style>
