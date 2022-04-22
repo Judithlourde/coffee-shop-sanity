62e6714b063f5b64ed929ea42cb031a0efa1f8e5
judith lourde <judith.lourde@gmail.com>
2022-04-22 21:38:10 +0200

    Move sanity client from store to sanity.js

--- app/src/store.js
+++ app/src/store.js
@@ -1,40 +1,21 @@
-import sanityClient from '@sanity/client';
-
-const sanity = sanityClient({
-	projectId: 'i90q2xi5', 
-	dataset: 'production',
-	apiVersion: '2022-04-04',	
-	useCdn: 'false'				
-});
-
 export default {
 	state() {
 		return {
 			shop: {
 				currency: 'NOK',
 				cart: [],
-				count: 0,
-				products: null
+				products: null,
 			},
-			loading: true,
 		}
 	},
 
 	getters: {
-		getLoadingStatus(state) {
-			return state.loading;
-		},
-
 		getProducts(state) {
 			return state.shop.products;
 		},
 
 		getAddedProducts(state) {
 			return state.shop.cart;
-		},
-
-		getCartLength(state) {
-			return state.shop.count;
 		},
 
 		getCurrency(state) {
@@ -52,68 +33,51 @@ export default {
 
 			// Checking the product already exist in the cart first, If it finds only increasing the count otherwise it push the product to the cart
 			// Checking the product exist by finding the index of the product and compare in cart products
-			if ( state.shop.cart.findIndex ( ({_id}) => _id === product._id ) !== -1 ) {
-				state.shop.cart.forEach(cartItem => {
-					if( cartItem.title === product.title ) {
-						state.shop.count ++;
-					}
-				});
+			const foundIndex = state.shop.cart.findIndex ( ({_id}) => _id === product._id );
+			
+			if(foundIndex !== -1) {
+				state.shop.cart[foundIndex].count += 1;
 			}
 			 else {
-					state.shop.cart.push(product);
-					state.shop.count ++;
+				 	const productWithCount = { 
+						 ...product, 
+						 count: 1 
+					};
+					
+					state.shop.cart.push(productWithCount);
 				}
-			console.log(product.title);
-			console.log(state.shop.cart)
 		},
 
 		// Remove the product from the cart
-		deleteFromCart(state, cartProduct) {
-			const productId = state.shop.cart.findIndex(product => product._id === cartProduct._id);  
-      		state.shop.cart.splice(productId, 1); 
-			state.shop.count --;		// Decreasing the count of products
-			console.log(state.shop.cart)
+		decreasingCountFromCart(state, index) {
+			if(state.shop.cart[index].count === 1) {
+				state.shop.cart.splice(index, 1); 
+			}  else {
+				state.shop.cart[index].count -= 1;
+			}	
 		},
 
-		// Increasing the product count
-		increasingCount(state, product) {
-			// state.shop.cart.forEach(item => {
-			// 	if(item._id === product._id) {
-				       
-			// 	}
-			// });
+		deleteProductFromCart(state, index) {
+			state.shop.cart.splice(index, 1);
 		}
 	},
 
 	actions: {
-		async fetchProducts({ state, commit }) {
-			const query = `
-				*[_type == $type] {
-					...,
-					image {
-						...,
-						asset-> { url }
-					}
-				}		
-			`
-			const params = {
-				type: 'product'
-			};
-
-			const result = await sanity.fetch(query, params);
-			state.loading = false;
+		setResult({ state, commit }, result) {			
 			commit('handleProducts', result);
 		},
 
 		addToCart({state, commit}, product) {
 			commit('addToCart', product);
-			commit('increasingCount', product)
 		},
 
-		deleteFromCart({state, commit}, cartProduct) {
-			commit('deleteFromCart', cartProduct);
-			commit('decreasingCount', cartProduct)
-		}
+		decreasingCountFromCart({state, commit}, index) {
+			commit('decreasingCountFromCart', index);
+		},
+
+		deleteProductFromCart({state, commit}, index) {
+			commit('deleteProductFromCart', index);
+		},
 	}
 }
 
